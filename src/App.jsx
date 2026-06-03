41786c4cc08a42056f7542bd9aa7004493ebfdb2
Wys <quyc2thaison@gmail.com>
2026-06-04 01:41:10 +0700

    class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,95 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { Component } from "react";
 import avatar from "./assets/471954194_1547032819292501_6390247624185970325_n.jpg";
 
+class Header extends Component {
+  constructor(props) {
+    super(props);
 
-function Header() {
-  const Student = {
-    name: "Nguyen Van Quy",
-    studentID: "HE194261",
-    class: "SE2004",
-    major: "Software Engineering - NodeJS",
-    ListOfHobbies: ["Football", "Music", "Traveling"],
+    this.state = {
+      isOnline: true,
+      darkMode: false,
+      likes: 0,
+    };
+
+    this.Student = {
+      name: "Nguyen Van Quy",
+      studentID: "HE194261",
+      class: "SE2004",
+      major: "Software Engineering - NodeJS",
+      ListOfHobbies: ["Football", "Music", "Traveling"],
+    };
+  }
+
+  componentDidMount() {
+    document.body.classList.toggle("dark-mode", this.state.darkMode);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.darkMode !== this.state.darkMode) {
+      document.body.classList.toggle("dark-mode", this.state.darkMode);
+    }
+  }
+
+  toggleOnlineStatus = () => {
+    this.setState((state) => ({ isOnline: !state.isOnline }));
   };
-  const [isOnline, setIsOnline] = useState(true);
-  const [darkMode, setDarkMode] = useState(false);
-  const [likes, setLikes] = useState(0);
 
-  useEffect(() => {
-    document.body.classList.toggle("dark-mode", darkMode);
-  }, [darkMode]);
-
-  const toggleOnlineStatus = () => {
-    setIsOnline((current) => !current);
+  toggleDarkMode = () => {
+    this.setState((state) => ({ darkMode: !state.darkMode }));
   };
 
-  const addLike = () => {
-    setLikes((current) => current + 1);
+  addLike = () => {
+    this.setState((state) => ({ likes: state.likes + 1 }));
   };
 
-  return (
-    <div className="App">
-      <h1><b>Student Profile</b></h1>
-      <img src={avatar} alt="Avatar" className="avatar" />
-            <p>Full name: {Student.name}</p>
-            <p>Student ID: {Student.studentID}</p>
-            <p>Class name: {Student.class}</p>
-            <p>Major: {Student.major}</p>
-            <p>List of Hobbies:</p>
-            <ul style={{ listStyleType: "none", paddingLeft: 0, margin: 0 }}>
-              {Student.ListOfHobbies.map((hobby, index) => (
-                <li key={index}>{hobby}</li>
-              ))}
-            </ul>
-            <p>Status: {isOnline ? "Online" : "Offline"}</p>
-      
+  render() {
+    const { isOnline, darkMode, likes } = this.state;
+    const { Student } = this;
 
-      <button className="primary-button" onClick={toggleOnlineStatus}>
-        {isOnline ? "Offline" : "Online"}
-      </button>
-      
-      <button className="primary-button" onClick={() => setDarkMode((current) => !current)}>
-        {darkMode ? "Light Mode" : "Dark Mode"}
-      </button>
+    return (
+      <div className="App">
+        <h1>
+          <b>Student Profile</b>
+        </h1>
+        <img src={avatar} alt="Avatar" className="avatar" />
+        <p>Full name: {Student.name}</p>
+        <p>Student ID: {Student.studentID}</p>
+        <p>Class name: {Student.class}</p>
+        <p>Major: {Student.major}</p>
+        <p>List of Hobbies:</p>
+        <ul style={{ listStyleType: "none", paddingLeft: 0, margin: 0 }}>
+          {Student.ListOfHobbies.map((hobby, index) => (
+            <li key={index}>{hobby}</li>
+          ))}
+        </ul>
+        <p>Status: {isOnline ? "Online" : "Offline"}</p>
 
-      <button className="primary-button" onClick={addLike}>
-        Like ({likes})
-      </button>
+        <button className="primary-button" onClick={this.toggleOnlineStatus}>
+          {isOnline ? "Offline" : "Online"}
+        </button>
 
-    </div>
-  );
+        <button className="primary-button" onClick={this.toggleDarkMode}>
+          {darkMode ? "Light Mode" : "Dark Mode"}
+        </button>
+
+        <button className="primary-button" onClick={this.addLike}>
+          Like ({likes})
+        </button>
+      </div>
+    );
+  }
 }
 
-function App() {
-  return (
-    <div>
-      <Header/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
 }
+
 export default App;
+
